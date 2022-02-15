2d65c32586332b1866423ec57f8b5df06fcff257
gbstrom <85531363+gbstrom@users.noreply.github.com>
2022-02-15 10:47:51 -0500

    put in the code to get movie card working

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { UserRegistrationService } from '../fetch-api-data.service'
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
-export class MovieCardComponent implements OnInit {
+export class MovieCardComponent {
+  movies: any[] = [];
+  constructor(public fetchApiData: UserRegistrationService) { }
 
-  constructor() { }
+ngOnInit(): void {
+  this.getMovies();
+}
 
-  ngOnInit(): void {
+getMovies(): void {
+  this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      console.log(this.movies);
+      return this.movies;
+    });
   }
-
 }

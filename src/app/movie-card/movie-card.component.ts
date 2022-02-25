6a01913e10e598565d6bdc6991fb7af2c9bb19f5
gbstrom <85531363+gbstrom@users.noreply.github.com>
2022-02-25 15:37:53 -0500

    got the favorite & unfavorite code working, tidied

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -23,7 +23,6 @@ export class MovieCardComponent {
     FavoriteMovies: [],
   }
   currentUsersFaves: any[] =[];
-  faveBoolean: boolean = false;
 
   constructor(
     public fetchApiData: UserRegistrationService,
@@ -34,12 +33,10 @@ export class MovieCardComponent {
 
   ngOnInit(): void {
     this.getUser();
-    this.getFavorites();
-    console.log("at the end of ngOnInit(), the value of currentUsersFaves is " + this.currentUsersFaves);
-    console.log(this.currentUsersFaves);
+    this.getMoviesAndFaves();
   };
 
-  async getUser() {
+  getUser() {
     // this code sets the user object to the user found in localStorage
     if (localStorage.getItem('user') != null) {
       let userData: any = localStorage.getItem('user');
@@ -47,30 +44,18 @@ export class MovieCardComponent {
       this.fetchApiData.getUser(username).subscribe((resp: any) => {
         this.user = resp;
       });
-  
     };
   }
 
-  async getFavorites() {
+  getMoviesAndFaves() {
     // this code sets the currentUsersFaves variable to the set of movies that the database has recorded as favorites for the user.
     this.fetchApiData.getAllMovies().subscribe((res: any) => {
       this.movies = res;
       this.movies.forEach((movie) => {
-      let element = {name: "monkey"}
         if (this.user.FavoriteMovies.includes(movie._id)) {
           this.currentUsersFaves.push(movie);
         }
       });
-    });
-    console.log('at the end of this.getFavorites(), the value of currentUsersFaves is ' + this.currentUsersFaves);
-    console.log(this.currentUsersFaves);
-  }
-
-  getMovies(): void {
-    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
-      this.movies = resp;
-      console.log(this.movies);
-      return this.movies;
     });
   }
 
@@ -112,73 +97,30 @@ export class MovieCardComponent {
 
   addToFavorites(movieId: string): void {
     this.fetchApiData.addFavoriteMovie(movieId).subscribe((resp: any) => {
-      this.getFavorites();
-      // this.itIsAFave(movieId);
-      console.log("the itIsAFave value of " + movieId + " is " + this.faveBoolean)
+      this.movies.forEach((movie) => {
+        if (movie._id == movieId) {
+          this.currentUsersFaves.push(movie);
+        }
+      });
       this.snackBar.open('Added to favorites', 'OK', { duration: 2000 });
     });
   }
 
   removeFromFavorites(movieId: string): void {
     this.fetchApiData.deleteFavoriteMovie(movieId).subscribe((resp: any) => {
-      this.fetchApiData.getAllMovies().subscribe((res: any) => {
-        this.movies = res;
-        this.movies.forEach((movie) => {
-          if (this.user.FavoriteMovies.includes(movie._id)) {
-            this.currentUsersFaves.push(movie);
-          }
-        });
-      });
+      const previousFavesWithDisfavoredMovieRemoved = this.currentUsersFaves.filter(movie => movie._id !== movieId);
+      this.currentUsersFaves = previousFavesWithDisfavoredMovieRemoved;
       this.snackBar.open('Removed from favorites', 'OK', { duration: 2000 });
     })
   }
 
-
-  // I tried to make the ngIf in the component work by linking it to this function, but it did not work.
-  // itIsAFave(movieId: string): any {
-  //   let faveIds = this.currentUsersFaves.map(function (fav: any) { return fav._id });
-  //   if (faveIds.includes(movieId)) {
-  //     this.faveBoolean = true;
-  //     return this.faveBoolean;
-  //   };
-  // }
-
   itIsAFave(movieId: string): any {
-    // As far as I can tell, this is good code, but still ngIf doesn't behave itself--
     const movieArray: any[] = this.currentUsersFaves;
-
     if(movieArray.some(movie => movie._id === movieId)){
-      console.log('itIsAFave claims that movieArray includes ' + movieId)
       return true;
     } else {
-      console.log('itIsAFave denies that gregsArray includes ' + movieId)
       return false;
     }
-
-    // This is all nonsense that I will delete eventually, that helped me troubleshoot.
-    // const movieArray: any[] = [{ _id: "60f1cc137a111c2a24f78e1b", Title: "Rear Window", Description: "A photogropher and his socialite girlfriend inadvertently come to suspect that one of their neighbors has murdered his wife in this Hitchcock classic."},
-    // { _id: "60f1cc667a111c2a24f78e1c", Title: "The 39 Steps", Description: "Through a sequence of unlikely circumstances a Canadian travelling in Britain becomes a murder suspect and has to flee across the country to find clues that will clear his name and expose a secret group of spies."},
-    // { _id: "60f1cda67a111c2a24f78e1f", Title: "The 40 Year Old Virgin", Description: "Hijinks ensue when the coworkers of a 40 year old salesman discover that he is still a virgin."},
-    // { _id: "60f1cb077a111c2a24f78e1a", Title: "North by Northwest", Description: "An advertising executive finds himself entangled by a case of mistaken identity in a dangerous world of espionage and adventure in one of Hitchcock's most beloved movies"},
-    // { _id: "60f1cd3b7a111c2a24f78e1e", Title: "Late Spring", Description: "A devoted daughter struggles to accept change when her father realizes that it is time for her to get married."},
-    // { _id: "60f1ca927a111c2a24f78e19", Title: "The Lego Batman Movie", Description: "Batman has to face his greatest fear to save Gotham City from the Joker while learning to work with the new police commissioner."}]
-    // const gregsArray: any[] = [{name: "Huey", species: "duck"}, {name: "Dewey", species: "duck"}, {name: "Louie", species: "duck"}];
-    // if(gregsArray.some(duck => duck.name === "Huey")){
-    //   console.log('itIsAFave claims that gregsArray includes Huey')
-    //   return true;
-    // } else {
-    //   console.log('itIsAFave denies that gregsArray includes Huey')
-    //   return false;
-    // }
-
-    // console.log('itIsAFave claims that it is ' + gregsArray.includes(7) + " that gregsArray includes 7");
-    // console.log("itIsAFave is running, and the ")
-    // if (this.currentUsersFaves.includes(movieId)) {
-    //   console.log("itIsAFave is true about " + movieId)
-    // } else {
-    //   console.log("itIsAFave is false about " + movieId + " and yet currentUsersFaves is");
-    //   console.log(this.currentUsersFaves);
-    // };
   }
 
   signOut(): void {
--- src/app/profile-view/profile-view.component.ts
+++ src/app/profile-view/profile-view.component.ts
@@ -7,6 +7,7 @@ import { UpdateInfoComponent } from '../update-info/update-info.component';
 import { DirectorCardComponent } from '../director-card/director-card.component';
 import { GenreCardComponent } from '../genre-card/genre-card.component';
 import { DescriptionCardComponent } from '../description-card/description-card.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
@@ -16,22 +17,53 @@ import { DescriptionCardComponent } from '../description-card/description-card.c
 })
 
 export class ProfileViewComponent implements OnInit {
-  favoriteMovies: any[] = [];
-  public user: any = {
+  movies: any[] = [];
+  user: any = {
     Username: '',
     Email: '',
     Birthday: '',
     FavoriteMovies: [],
   }
+  currentUsersFaves: any[] =[];
+
   
     constructor(
       public fetchApiData: UserRegistrationService,
       private router: Router,
+      public snackBar: MatSnackBar,
       public dialog: MatDialog,
       // @Inject(MAT_DIALOG_DATA)
       
     ) { }
   
+    ngOnInit(): void {
+      this.getUser();
+      this.getMoviesAndFaves();
+    }
+
+    getUser() {
+      // this code sets the user object to the user found in localStorage
+      if (localStorage.getItem('user') != null) {
+        let userData: any = localStorage.getItem('user');
+        let username = JSON.parse(userData).Username;
+        this.fetchApiData.getUser(username).subscribe((resp: any) => {
+          this.user = resp;
+        });
+      };
+    }
+  
+    getMoviesAndFaves() {
+      // this code sets the currentUsersFaves variable to the set of movies that the database has recorded as favorites for the user.
+      this.fetchApiData.getAllMovies().subscribe((res: any) => {
+        this.movies = res;
+        this.movies.forEach((movie) => {
+          if (this.user.FavoriteMovies.includes(movie._id)) {
+            this.currentUsersFaves.push(movie);
+          }
+        });
+      });
+    }
+
     goToMovieCard(): void {
       this.router.navigate(['movies']);
     }
@@ -40,23 +72,6 @@ export class ProfileViewComponent implements OnInit {
       this.dialog.open(UpdateInfoComponent, {
         width: '500px'
       });
-    }
-
-    // this function runs through the list of all movies checking to see whether their _id matches the _ids in data.FavoriteMovies. Whenever there's a match, the movie,
-    // with all its details, is pushed into this.favoriteMovies
-    getFaves(): Array<Object> {
-      let movies: any[] = [];
-      // let userData: any = localStorage.getItem('user');
-      // this.data = JSON.parse(userData);
-      this.fetchApiData.getAllMovies().subscribe((res: any) => {
-        movies = res;
-        movies.forEach((movie) => {
-          if (this.user.FavoriteMovies.includes(movie._id)) {
-            this.favoriteMovies.push(movie);
-          }
-        });
-      });
-      return this.favoriteMovies;
     }
 
     openDirector(name: string, bio: string, birth: string, death: string): void {
@@ -91,15 +106,34 @@ export class ProfileViewComponent implements OnInit {
       });
     }
 
-    ngOnInit(): void {
-      if (localStorage.getItem('user') != null) {
-        let userData: any = localStorage.getItem('user');
-        this.user = JSON.parse(userData);
-        console.log(this.user)
-      };
-      this.getFaves();
-
+    addToFavorites(movieId: string): void {
+      this.fetchApiData.addFavoriteMovie(movieId).subscribe((resp: any) => {
+        this.movies.forEach((movie) => {
+          if (movie._id == movieId) {
+            this.currentUsersFaves.push(movie);
+          }
+        });
+        this.snackBar.open('Added to favorites', 'OK', { duration: 2000 });
+      });
     }
+  
+    removeFromFavorites(movieId: string): void {
+      this.fetchApiData.deleteFavoriteMovie(movieId).subscribe((resp: any) => {
+        const previousFavesWithDisfavoredMovieRemoved = this.currentUsersFaves.filter(movie => movie._id !== movieId);
+        this.currentUsersFaves = previousFavesWithDisfavoredMovieRemoved;
+        this.snackBar.open('Removed from favorites', 'OK', { duration: 2000 });
+      })
+    }
+  
+
+    itIsAFave(movieId: string): any {
+      const movieArray: any[] = this.currentUsersFaves;
+      if(movieArray.some(movie => movie._id === movieId)){
+        return true;
+      } else {
+        return false;
+      }
+    }  
 
     signOut(): void {
       this.router.navigate(['welcome']);
